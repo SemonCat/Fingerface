b632715b618dd0a836c7cfe75879563beed73cef
topjohnwu <topjohnwu@gmail.com>
2019-11-01 05:18:26 -0400

    Several small improvements
    - Enable Proguard
    - Remove AppCompatActivity

--- app/src/main/java/com/edison/fingerface/MainActivity.kt
+++ app/src/main/java/com/edison/fingerface/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.edison.fingerface
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
